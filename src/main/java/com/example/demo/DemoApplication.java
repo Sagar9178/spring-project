19c27e03bfef0239fbb37c15ae0e3ee8c71f5538
Sagar <sagarkalia6371@gmail.com>
2024-11-23 08:25:37 +0530

    print the variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-int i=0;
+		int i=0;
+		System.out.println(i);
 
 	}
 
